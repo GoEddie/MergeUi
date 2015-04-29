71380f7f1c15ac0bad51b18c73e4e19f55365164
Ed Elliott <ed@agilesql.club>
2015-04-29 23:51:27 +0100

    new wpf layout with drop downs along the top, needs implementing

---
+++ src/AgileSqlClub.MergeUiPackage/UI/MergeUi.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace AgileSqlClub.MergeUI.UI
+{
+    /// <summary>
+    /// Interaction logic for MergeUi.xaml
+    /// </summary>
+    public partial class MergeUi : UserControl, IStatus
+    {
+        public MergeUi()
+        {
+            InitializeComponent();
+        }
+
+        public void SetStatus(string message)
+        {
+            
+        }
+    }
+}
--- src/AgileSqlClub.MergeUiPackage/UI/MyToolWindow.cs
+++ src/AgileSqlClub.MergeUiPackage/UI/MyToolWindow.cs
@@ -34,9 +34,11 @@ namespace AgileSqlClub.MergeUI.UI
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on 
             // the object returned by the Content property.
-            var control = base.Content = new MyControl();
-            
-                       
+            // var control = base.Content = new MyControl();
+
+            var control = base.Content = new MergeUi();
+
+
         }
 
     }
